d9213061619ab7517f1809eca45b404d2886ee45
viragvegh <88virag@gmail.com>
2022-06-14 09:49:24 +0200

    created new files

---
+++ hello.py
@@ -0,0 +1,25 @@
+x = 0
+for x in range(10):
+    if x % 2 == 0:
+        print(x)
+
+fruits = ["apple", "banana", "cherry"]
+for x in fruits:
+    print(x)
+print("asd")
+
+for x in range(2, 30, 3):
+    print(x)
+
+for x in range(2, 6):
+ print(x)
+else:
+ print('Finally finished!')
+
+ for x in 'banana':
+   print(x)
+
+fruits.add("ribizli")
+
+for x in fruits:
+  print(x)
